0fb0cb13636f00f988d1811a23f74eab658a1d2a
bird <raju-tati>
2023-03-14 15:41:19 +0530

    commit one

---
+++ lib/qq.pm
@@ -0,0 +1,34 @@
+package qq;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.06';
+
+1;
+__END__
+
+=head1 NAME
+
+qq - command line journal app
+
+=head1 SYNOPSIS
+
+  $> qq
+
+=head1 DESCRIPTION
+
+Type 'qq' at the command line to start the application
+
+=head1 AUTHOR
+
+Rajkumar Reddy
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2022 by Rajkumar Reddy. All rights reserved.
+
+Open Source.
+
+
+=cut
